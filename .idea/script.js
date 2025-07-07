285e8aa22225246d80c5cd1f65ed9c9feb03ba2e
KCP-IanMcK <pit.huwiler@bluewin.ch>
2025-07-07 20:21:16 +0200

    Extract constants, add better error handling

--- .idea/script.js
+++ .idea/script.js
@@ -2,6 +2,9 @@ const masterInput = document.getElementById("master");
 const websiteInput = document.getElementById("url");
 const submitBtn = document.getElementById("btn1");
 const toggleBtn = document.getElementById("btn2");
+const hashField = document.getElementById("password");
+
+const passwordLength = 24;
 
 const specialChars = ['?', '!', '*', '+', '&', '=', '/', '@', '#', '$', '<', '>', '(', ')', '-', '_', '.', '%'];
 
@@ -11,7 +14,6 @@ submitBtn.addEventListener("click", handleEvent);
 
 toggleBtn.addEventListener("click", function(event) {
     event.preventDefault();
-    const hashField = document.getElementById("password");
     if (hashField.type === "password") {
         toggleBtn.innerText = "Hide";
         hashField.type = "text";
@@ -29,9 +31,10 @@ async function handleEvent(event) {
     const hashText = await runGeneration();
     navigator.clipboard.writeText(hashText)
                 .then(() => {
-                showBanner("Copied to Clipboard");
+                    showBanner("Copied to Clipboard", false);
                 })
                 .catch(err => {
+                    showBanner("Copy to Clipboard failed", true);
                     console.error("Error when copying to clipboard:", err);
                 });
 }
@@ -44,8 +47,7 @@ async function runGeneration(event) {
 
     const hashText = await getPasswordHash(masterPW + websiteURL);
 
-    const outputField = document.getElementById("password");
-    outputField.value = hashText;
+    hashField.value = hashText;
 
     return hashText;
 }
@@ -75,14 +77,14 @@ async function getPasswordHash(pwUrlCombination) {
     const data = encoder.encode(pwUrlCombination);
     const hashBuffer = await crypto.subtle.digest('SHA-256', data);
     const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const specialCharPos = hashArray[0] % 24;
-    let lowerCaseCharPos = hashArray[1] % 24;
-    let upperCaseCharPos = hashArray[2] % 24;
-    let numberCharPos = hashArray[3] % 16;
-    const specialChar = specialChars[hashArray[15] % 18];
+    const specialCharPos = hashArray[0] % passwordLength;
+    let lowerCaseCharPos = hashArray[1] % passwordLength;
+    let upperCaseCharPos = hashArray[2] % passwordLength;
+    let numberCharPos = hashArray[3] % passwordLength;
+    const specialChar = specialChars[hashArray[15] % specialChars.length];
 
     let hashText = btoa(String.fromCharCode(...hashArray));
-    hashText = hashText.slice(0, 24);
+    hashText = hashText.slice(0, passwordLength);
 
     while (!/[+=\/!?*&@#$<>()_.%-]/.test(hashText) || !/[a-z]/.test(hashText) || !/[A-Z]/.test(hashText) || !/[0-9]/.test(hashText)) {
         if (!/[+=\/!]/.test(hashText)) {
@@ -91,21 +93,21 @@ async function getPasswordHash(pwUrlCombination) {
 
         if (!/[a-z]/.test(hashText)) {
             if(lowerCaseCharPos === specialCharPos) {
-                lowerCaseCharPos = lowerCaseCharPos === 23 ? 0 : lowerCaseCharPos + 1;
+                lowerCaseCharPos = lowerCaseCharPos === passwordLength -1 ? 0 : lowerCaseCharPos + 1;
             }
             hashText = replaceAt(hashText, lowerCaseCharPos , "a");
         }
 
         if (!/[A-Z]/.test(hashText)) {
             if(upperCaseCharPos === lowerCaseCharPos || upperCaseCharPos === specialCharPos) {
-                upperCaseCharPos = upperCaseCharPos === 23 ? 0 : upperCaseCharPos + 1;
+                upperCaseCharPos = upperCaseCharPos === passwordLength -1 ? 0 : upperCaseCharPos + 1;
             }
             hashText = replaceAt(hashText, upperCaseCharPos , "A");
         }
 
         if (!/[0-9]/.test(hashText)) {
              if(numberCharPos === lowerCaseCharPos || numberCharPos === specialCharPos || numberCharPos === upperCaseCharPos) {
-                numberCharPos = numberCharPos === 23 ? 0 : numberCharPos + 1;
+                numberCharPos = numberCharPos === passwordLength -1 ? 0 : numberCharPos + 1;
              }
              hashText = replaceAt(hashText, numberCharPos , "1");
         }
@@ -113,13 +115,18 @@ async function getPasswordHash(pwUrlCombination) {
     return hashText;
 }
 
-function showBanner(message, duration = 2000) {
+function showBanner(message, isException, duration = 2000) {
   const banner = document.getElementById("banner");
   banner.textContent = message;
   banner.classList.remove("hidden");
 
+  if (isException) {
+    banner.classList.add("exception");
+  }
+
   setTimeout(() => {
     banner.classList.add("hidden");
+    banner.classList.remove("exception");
   }, duration);
 }
 
